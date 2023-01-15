90468129c339da478d062f14fc9ea478c6ef22e5
minami-11 <madk0te@yandex.ru>
2023-01-15 15:29:11 +0500

    Feat and refactor: add gcd game in game.engine.py
    Add new game "GCD" (greatest common divisor) in game.engine.py :
    -add gcd_checker function. Euclidean algorithm for gcd in it. Returns
    GCD number
    -add gcd link in gamelogic_frame.py
    -add gcd question generator in game_generator function
    
    Add docstring in calc_checker function.

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -6,7 +6,8 @@ def even_odd_checker(number: int) -> str:
     return ['yes', 'no'][number % 2]
 
 
-def calc_checker(math_string: str):
+def calc_checker(math_string: str) -> int:
+    '''Сonverts math_string to a mathematical operation'''
     a, math_operator, b = math_string.split()
     action_list = {'+': int(a) + int(b),
                    '-': int(a) - int(b),
@@ -14,12 +15,25 @@ def calc_checker(math_string: str):
     return action_list.get(math_operator)
 
 
+def gcd_checker(numb_string: str):
+    '''Euclidean algorithm for gcd'''
+    a, b = map(int, numb_string.split())
+    while a != b:
+        if a >= b:
+            a = a - b
+        else:
+            b = b - a
+    return a
+
+
 def game_generator(game_name: str) -> (str | int):
+    '''Generates questions for games'''
     math_actions = ['+', '-', '*']
     a = randint(0, 100)
     b = randint(0, 100)
     game_pack = {'even_odd': a,
-                 'calc': ' '.join(map(str, [a, choice(math_actions), b]))}
+                 'calc': ' '.join(map(str, [a, choice(math_actions), b])),
+                 'gcd': f'{a} {b}'}
     return game_pack.get(game_name)
 
 
@@ -33,6 +47,7 @@ def gamelogic_frame(game_name: str) -> str:
         match game_name:
             case 'even_odd': game_correct_answer = even_odd_checker(guess)
             case 'calc': game_correct_answer = str(calc_checker(guess))
+            case 'gcd': game_correct_answer = str(gcd_checker(guess))
         if game_correct_answer == user_answer:
             answer_pack['correct'] += 1
             print('Correct!')

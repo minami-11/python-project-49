26c048213360fbf252b0666cf32e73b74183b113
minami-11 <madk0te@yandex.ru>
2023-01-16 15:24:57 +0500

    Feat and refactor: add progression in game.engine.py
    Add arithmetic progression game in gamelogic_frame. Add progress_che-
    cker(). Add 'progress' in game_generator (generates question for games).
    
    Add if/elif part in gcd_checker(). Because function didn't work with 0.
    But we can divide zero by any number. For example 0/10 = 0. That's why
    gcd(0,10) should return 10.
    
    In game_generator reduce the upper level of randint in a and b variables.

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -18,6 +18,10 @@ def calc_checker(math_string: str) -> int:
 def gcd_checker(numb_string: str):
     '''Euclidean algorithm for gcd'''
     a, b = map(int, numb_string.split())
+    if a == 0 and b == 0:
+        return 0
+    elif a == 0 or b == 0:
+        return max(a, b)
     while a != b:
         if a >= b:
             a = a - b
@@ -26,14 +30,29 @@ def gcd_checker(numb_string: str):
     return a
 
 
+def progression_checker() -> list:
+    '''Random arithmetic progression -> [progression str, correct answer]'''
+    progress_length = randint(5, 10)
+    progress_point = randint(0, 50)
+    progress_step = randint(2, 10)
+    question = []
+    for _ in range(progress_length):
+        question.append(str(progress_point))
+        progress_point += progress_step
+    guess_numb = choice(question)
+    question[question.index(guess_numb)] = '..'
+    return [' '.join(question)] + [str(guess_numb)]
+
+
 def game_generator(game_name: str) -> (str | int):
     '''Generates questions for games'''
     math_actions = ['+', '-', '*']
-    a = randint(0, 100)
-    b = randint(0, 100)
+    a = randint(0, 50)
+    b = randint(0, 50)
     game_pack = {'even_odd': a,
                  'calc': ' '.join(map(str, [a, choice(math_actions), b])),
-                 'gcd': f'{a} {b}'}
+                 'gcd': f'{a} {b}',
+                 'progress': progression_checker()}
     return game_pack.get(game_name)
 
 
@@ -41,13 +60,17 @@ def gamelogic_frame(game_name: str) -> str:
     '''Game engine'''
     answer_pack = {'correct': 0, 'wrong': 0}
     while all([answer_pack['correct'] < 3, answer_pack['wrong'] < 1]):
-        guess = game_generator(game_name)
+        if game_name == 'progress':
+            guess, progress_answer = game_generator(game_name)
+        else:
+            guess = game_generator(game_name)
         print(f'Question: {guess}')
         user_answer = prompt.string('Your answer: ')
         match game_name:
             case 'even_odd': game_correct_answer = even_odd_checker(guess)
             case 'calc': game_correct_answer = str(calc_checker(guess))
             case 'gcd': game_correct_answer = str(gcd_checker(guess))
+            case 'progress': game_correct_answer = progress_answer
         if game_correct_answer == user_answer:
             answer_pack['correct'] += 1
             print('Correct!')

b0e62c784bacf63e6b39a3c8c431935a7ca0ab5d
minami-11 <madk0te@yandex.ru>
2023-01-17 20:16:23 +0500

    Refactor: big refactor of game_engine.py
    Move module imports to brain_games/games/__init__.py . Add import of
    games package and brain_games.cli
    
    Rename game_generator() to question_generator() and refactor it.
    Move each game question code to exact file (even_odd to eve_odd.py,
    prime to prime.py, etc.)
    
    Rename game_answer() to answer_generator and refactor it. Change
    return for 'progress' from return None to str(games.progress_answ(..))
    
    Rename gamelogic_frame to game_frame and refactor it. Integrate: wel-
    come_user(), game_start_message().
    
    Integrate win_or_fall_message() into game_frame().

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -1,59 +1,53 @@
-from random import randint, choice
 import prompt
-from brain_games.games.even_odd import even_odd_checker
-from brain_games.games.calculate import calc_checker
-from brain_games.games.gcd import gcd_checker
-from brain_games.games.progression import progression_checker
-from brain_games.games.prime import prime_checker
+from brain_games.cli import welcome_user
+import brain_games.games as games
 
 
-def game_generator(game_name: str) -> (str | int):
-    '''Generates questions for games'''
-    math_actions = ['+', '-', '*']
-    a = randint(0, 50)
-    b = randint(0, 50)
-    game_pack = {'even_odd': a,
-                 'calc': ' '.join(map(str, [a, choice(math_actions), b])),
-                 'gcd': f'{a} {b}',
-                 'progress': progression_checker(),
-                 'prime': a}
-    return game_pack.get(game_name)
-
-
-def game_answer(game_name: str, guess: (str | int)) -> str:
-    '''Returns correct answer for each game'''
+def game_start_message(game_name: str):
+    '''Each game enter message'''
     match game_name:
-        case 'even_odd': return even_odd_checker(guess)
-        case 'calc': return str(calc_checker(guess))
-        case 'gcd': return str(gcd_checker(guess))
-        case 'progress': return None
-        case 'prime': return prime_checker(guess)
+        case 'even_odd': return games.even_st()
+        case 'calc': return games.calc_st()
+        case 'gcd': return games.gcd_st()
+        case 'progress': return games.progress_st()
+        case 'prime': return games.prime_st()
 
 
-def gamelogic_frame(game_name: str) -> str:
-    '''Game engine'''
+def question_generator(game_name: str) -> str:
+    match game_name:
+        case 'even_odd': return games.even_q()
+        case 'calc': return games.calc_q()
+        case 'gcd': return games.gcd_q()
+        case 'progress': return games.progress_q()
+        case 'prime': return games.prime_q()
+
+
+def answer_generator(game_name: str, guess: str):
+    match game_name:
+        case 'even_odd': return games.even_answ(guess)
+        case 'calc': return str(games.calc_answ(guess))
+        case 'gcd': return str(games.gcd_answ(guess))
+        case 'progress': return str(games.progress_answ(guess))
+        case 'prime': return games.prime_answ(guess)
+
+
+def game_frame(game_name: str):
+    USER = welcome_user()
+    print(game_start_message(game_name))
     answer_pack = {'correct': 0, 'wrong': 0}
     while all([answer_pack['correct'] < 3, answer_pack['wrong'] < 1]):
-        if game_name == 'progress':
-            guess, game_correct_answer = game_generator(game_name)
-        else:
-            guess = game_generator(game_name)
-            game_correct_answer = game_answer(game_name, guess)
+        guess = question_generator(game_name)
         print(f'Question: {guess}')
         user_answer = prompt.string('Your answer: ')
-        if game_correct_answer == user_answer:
+        correct_answer = answer_generator(game_name, guess)
+        if correct_answer == user_answer:
             answer_pack['correct'] += 1
             print('Correct!')
         else:
             answer_pack['wrong'] += 1
             print(f"'{user_answer}' is wrong answer ;(. "
-                  f"Correct answer was '{game_correct_answer}'.")
-    return "Win" if answer_pack.get('correct') == 3 else "Fall"
-
-
-def win_or_fall_message(func_result: str, USER: str) -> None:
-    '''User gets win or lose message'''
-    if func_result == "Win":
+                  f"Correct answer was '{correct_answer}'.")
+    if answer_pack['correct'] == 3:
         print(f'Congratulations, {USER}!')
     else:
         print(f"Let's try again, {USER}!")

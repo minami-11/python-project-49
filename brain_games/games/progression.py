b40d7b72b91c670ada62586e83bc81ae8e080501
minami-11 <madk0te@yandex.ru>
2023-01-17 20:07:22 +0500

    Feat and refactor: progression.py new functions
    Add new function: start_message(). Rename progression_checker() to
    question(). Exlude from question() correct answer returning.
    
    Add new fuction: progression_checker(). Gets string with arithmetic
    progression with missing number and returns missing number in progres-
    sion.

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -1,15 +1,34 @@
 from random import randint, choice
 
 
-def progression_checker() -> list:
-    '''Random arithmetic progression -> [progression str, correct answer]'''
+def start_message():
+    return 'What number is missing in the progression?'
+
+
+def question():
+    '''Random arithmetic progression'''
     progress_length = randint(5, 10)
     progress_point = randint(0, 50)
     progress_step = randint(2, 10)
-    question = []
+    arithm_progress = []
     for _ in range(progress_length):
-        question.append(str(progress_point))
+        arithm_progress.append(str(progress_point))
         progress_point += progress_step
-    guess_numb = choice(question)
-    question[question.index(guess_numb)] = '..'
-    return [' '.join(question)] + [str(guess_numb)]
+    guess_numb = choice(arithm_progress)
+    arithm_progress[arithm_progress.index(guess_numb)] = '..'
+    return ' '.join(arithm_progress)
+
+
+def progression_checker(string: str):
+    '''Returns missing number in progression'''
+    progress = string.split()
+    x = progress.index('..')
+    count = 0
+    while True:
+        if progress[count].isdigit() and progress[count + 1].isdigit():
+            step = int(progress[count + 1]) - int(progress[count])
+            break
+        count += 1
+    if x == len(progress) - 1:
+        return int(progress[x - 1]) + step
+    return (int(progress[x + 1]) - step, int(progress[x - 1]) + step)[bool(x)]

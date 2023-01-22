eed662062b0e57ca9c1252ee3cca7cc7a28760c5
minami-11 <madk0te@yandex.ru>
2023-01-22 23:13:58 +0500

    Refactor: brain_game.py due to review_refactor v.1.0
    Due to review_refactor v.1.0 remove import brain_games.games as
    games.
    
    Remove functions:
    -game_start_message, -question_generator, -answer_generator
    All these functions are unnecessary, since this commit, cause we
    import module path directly in game_frame.
    
    Change game_frame argument from game_name to game_module.
    Fix start message, guess and correct_answer after that.
    
    Add docstring.

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -1,45 +1,17 @@
 import prompt
 from brain_games.cli import welcome_user
-import brain_games.games as games
 
 
-def game_start_message(game_name: str):
-    '''Each game enter message'''
-    match game_name:
-        case 'even_odd': return games.even_st()
-        case 'calc': return games.calc_st()
-        case 'gcd': return games.gcd_st()
-        case 'progress': return games.progress_st()
-        case 'prime': return games.prime_st()
-
-
-def question_generator(game_name: str) -> str:
-    match game_name:
-        case 'even_odd': return games.even_q()
-        case 'calc': return games.calc_q()
-        case 'gcd': return games.gcd_q()
-        case 'progress': return games.progress_q()
-        case 'prime': return games.prime_q()
-
-
-def answer_generator(game_name: str, guess: str):
-    match game_name:
-        case 'even_odd': return games.even_answ(guess)
-        case 'calc': return str(games.calc_answ(guess))
-        case 'gcd': return str(games.gcd_answ(guess))
-        case 'progress': return str(games.progress_answ(guess))
-        case 'prime': return games.prime_answ(guess)
-
-
-def game_frame(game_name: str):
+def game_frame(game_module):
+    '''Rules > question > answer > check > result'''
     USER = welcome_user()
-    print(game_start_message(game_name))
+    print(game_module.start_message())
     answer_pack = {'correct': 0, 'wrong': 0}
     while all([answer_pack['correct'] < 3, answer_pack['wrong'] < 1]):
-        guess = question_generator(game_name)
+        guess = game_module.question()
         print(f'Question: {guess}')
         user_answer = prompt.string('Your answer: ')
-        correct_answer = answer_generator(game_name, guess)
+        correct_answer = game_module.checker(guess)
         if correct_answer == user_answer:
             answer_pack['correct'] += 1
             print('Correct!')

a2cb93ee9ed12d33ccbcc8897e9460e42ed76334
minami-11 <madk0te@yandex.ru>
2023-01-25 14:25:39 +0500

    Refactor: calculate due to review_refactor v.2.0
    Due to review_refactor v.2.0 function start_message() convert to cons-
    tant START_MESSAGE.
    
    Combine two functions (question(), checker()) into one:
    - question_checker(). Randint limits move to constants. Refactor ques-
    tion to f-string.
    
    Question_checker() return list [question, answer].

--- brain_games/games/calculate.py
+++ brain_games/games/calculate.py
@@ -1,22 +1,20 @@
 from random import randint, choice
 
 
-def start_message():
-    return 'What is the result of the expression?'
-
-
-def question() -> str:
-    '''Generates random game question'''
-    a = randint(0, 50)
-    b = randint(0, 50)
+def question_checker() -> list:
+    '''Generates random math expression and it's result'''
+    a = randint(MIN_LIM, MAX_LIM)
+    b = randint(MIN_LIM, MAX_LIM)
     math_actions = ['+', '-', '*']
-    return ' '.join(map(str, [a, choice(math_actions), b]))
-
-
-def checker(math_string: str) -> str:
-    '''Converts math_string to a mathematical operation'''
-    a, math_operator, b = math_string.split()
+    math_operator = choice(math_actions)
+    question = f'{a} {math_operator} {b}'
     action_list = {'+': int(a) + int(b),
                    '-': int(a) - int(b),
                    '*': int(a) * int(b)}
-    return str(action_list.get(math_operator))
+    answer = str(action_list.get(math_operator))
+    return [question, answer]
+
+
+START_MESSAGE = 'What is the result of the expression?'
+MIN_LIM = 0
+MAX_LIM = 50

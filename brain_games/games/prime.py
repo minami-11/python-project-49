7a9f78bf50e05adffc86a30ad0dfe35335b77985
minami-11 <madk0te@yandex.ru>
2023-01-25 14:39:12 +0500

    Refactor: prime due to review_refactor v.2.0
    Due to review_refactor v.2.0 function start_message() convert to cons-
    tant START_MESSAGE. Randint limits move to constants.
    
    Combine two functions (question(), checker()) into one: - question_
    checker(). Replace the previous prime code section with prime list.
    Question_checker() return list [question, answer].

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -1,27 +1,15 @@
 from random import randint
 
 
-def start_message():
-    return 'Answer "yes" if given number is prime. Otherwise answer "no".'
+def question_checker() -> list:
+    '''Generates random numb and returns Yes if prime, else No'''
+    question = randint(MIN_LIM, MAX_LIM)
+    prime_list = (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47,
+                  53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103)
+    answer = 'yes' if question in prime_list else 'no'
+    return [question, answer]
 
 
-def question():
-    return randint(0, 50)
-
-
-def checker(number: int) -> str:
-    '''Prime or not'''
-    match number:
-        case 1: return 'no'
-        case 2: return 'yes'
-    divider = 2
-    result = []
-    while True:
-        if number % divider == 0:
-            result.append(divider)
-        elif divider > number / 2:
-            break
-        if len(result) > 0:
-            return 'no'
-        divider += 1
-    return 'yes'
+START_MESSAGE = 'Answer "yes" if given number is prime. Otherwise answer "no".'
+MIN_LIM = 0
+MAX_LIM = 50

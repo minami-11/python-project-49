6075770678a496a940067816e4c7c3b868d603ff
minami-11 <madk0te@yandex.ru>
2023-01-25 14:47:11 +0500

    Refactor: game_engine due to review_refactor v.2.0
    Due to review_refactor v.2.0 imported function start_message() change
    to imported constant START_MESSAGE.
    
    Answer_pack with 'Correct' and 'Wrong' keys change to CORRECT and WRONG
    constants.
    
    Add question_and_answer variable. Game_module.question change to questi-
    on_and_answer[0]. Game_module.checker change to question_and_answer[1]

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -5,21 +5,23 @@ from brain_games.cli import welcome_user
 def game_frame(game_module):
     '''Rules > question > answer > check > result'''
     USER = welcome_user()
-    print(game_module.start_message())
-    answer_pack = {'correct': 0, 'wrong': 0}
-    while all([answer_pack['correct'] < 3, answer_pack['wrong'] < 1]):
-        guess = game_module.question()
+    print(game_module.START_MESSAGE)
+    CORRECT = 0
+    WRONG = 0
+    while all([CORRECT < 3, WRONG < 1]):
+        question_and_answer = game_module.question_checker()
+        guess = question_and_answer[0]
         print(f'Question: {guess}')
         user_answer = prompt.string('Your answer: ')
-        correct_answer = game_module.checker(guess)
+        correct_answer = question_and_answer[1]
         if correct_answer == user_answer:
-            answer_pack['correct'] += 1
+            CORRECT += 1
             print('Correct!')
         else:
-            answer_pack['wrong'] += 1
+            WRONG += 1
             print(f"'{user_answer}' is wrong answer ;(. "
                   f"Correct answer was '{correct_answer}'.")
-    if answer_pack['correct'] == 3:
+    if CORRECT == 3:
         print(f'Congratulations, {USER}!')
     else:
         print(f"Let's try again, {USER}!")

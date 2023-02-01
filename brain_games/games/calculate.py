77f8b71375753c58c206e37d5d48b1b76b7308e7
minami-11 <madk0te@yandex.ru>
2023-02-01 17:58:54 +0500

    Refactor: calculate due to review_refactor v.2.0
    Due to review_refactor v.3.0 move code part from question_checker func-
    tion to new function: string_to_math_converter.

--- brain_games/games/calculate.py
+++ brain_games/games/calculate.py
@@ -6,6 +6,14 @@ MIN_LIM = 0
 MAX_LIM = 50
 
 
+def string_to_math_converter(a: int, b: int, math_operator: str):
+    '''Converts string to math expression with end result'''
+    action_list = {'+': int(a) + int(b),
+                   '-': int(a) - int(b),
+                   '*': int(a) * int(b)}
+    return action_list.get(math_operator)
+
+
 def question_checker() -> list:
     '''Generates random math expression and it's result'''
     a = randint(MIN_LIM, MAX_LIM)
@@ -13,8 +21,5 @@ def question_checker() -> list:
     math_actions = ['+', '-', '*']
     math_operator = choice(math_actions)
     question = f'{a} {math_operator} {b}'
-    action_list = {'+': int(a) + int(b),
-                   '-': int(a) - int(b),
-                   '*': int(a) * int(b)}
-    answer = str(action_list.get(math_operator))
+    answer = str(string_to_math_converter(a, b, math_operator))
     return [question, answer]

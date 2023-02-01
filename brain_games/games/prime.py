f6efa0303735b3ff3ea920258fc82ba8c8293155
minami-11 <madk0te@yandex.ru>
2023-02-01 18:14:11 +0500

    Refactor: prime  due to review_refactor v.3.0
    Due to review_refactor v.3.0 add new function prime.
    
    In question_checker function replace previous code part (prime_list)
    with calling prime function from above.

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -6,10 +6,18 @@ MIN_LIM = 0
 MAX_LIM = 50
 
 
+def prime(number: int) -> str:
+    '''Yes if prime, else no'''
+    match number:
+        case 1: return 'no'
+        case 2: return 'yes'
+    if all(number % divider for divider in range(2, int(number / 2))):
+        return 'yes'
+    return 'no'
+
+
 def question_checker() -> list:
     '''Generates random numb and returns Yes if prime, else No'''
     question = randint(MIN_LIM, MAX_LIM)
-    prime_list = (2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47,
-                  53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103)
-    answer = 'yes' if question in prime_list else 'no'
+    answer = prime(question)
     return [question, answer]

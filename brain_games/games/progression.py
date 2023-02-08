f705ef0b8ad7aee8a761c45eec82886019fcf379
minami-11 <madk0te@yandex.ru>
2023-02-08 23:19:22 +0500

    Refactor: progression due to review_refactor v.4.0
    Due to review_refactor v.4.0 rename constants names."Question_checker"
    function name rename to "make_question_and_answer", code part from
    question_checker function move to new function "cut_number_from_progres-
    sion"

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -2,25 +2,29 @@ from random import randint, choice
 
 
 START_MESSAGE = 'What number is missing in the progression?'
-MIN_LIM = 0
-MAX_LIM = 50
+MIN_LIMIT = 0
+MAX_LIMIT = 50
 LENGTH_MIN = 5
 LENGTH_MAX = 10
 STEP_MIN = 2
 STEP_MAX = 10
 
 
-def question_checker() -> list:
+def cut_number_from_progression(progression: list) -> list:
+    '''Cut number from progression and return them back'''
+    guess_number = choice(progression)
+    progression[progression.index(guess_number)] = '..'
+    return [' '.join(progression), str(guess_number)]
+
+
+def make_question_and_answer() -> list:
     '''Generates random arithmetic progression with missing number'''
     progress_length = randint(LENGTH_MIN, LENGTH_MAX)
-    progress_point = randint(MIN_LIM, MAX_LIM)
+    progress_point = randint(MIN_LIMIT, MAX_LIMIT)
     progress_step = randint(STEP_MIN, STEP_MAX)
     arithm_progress = []
     for _ in range(progress_length):
         arithm_progress.append(str(progress_point))
         progress_point += progress_step
-    guess_numb = choice(arithm_progress)
-    arithm_progress[arithm_progress.index(guess_numb)] = '..'
-    question = ' '.join(arithm_progress)
-    answer = str(guess_numb)
+    question, answer = cut_number_from_progression(arithm_progress)
     return [question, answer]

213525edf86e561706d2b5c356e56454ea42b468
minami-11 <madk0te@yandex.ru>
2023-02-08 23:24:55 +0500

    Refactor: game_engine due to review_refactor v.4.0
    Due to review_refactor v.4.0 change "game_frame" function name to
    "create_gameplay". User part replace with code part from cli.py.
    
    Rename "CORRECT" to "Correct", "user" to "name".  Refactor guess, cor-
    rect_answer unpack.

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -1,25 +1,24 @@
 import prompt
-from brain_games.cli import welcome_user
 
 
-def game_frame(game_module):
+def create_gameplay(game_module):
     '''Rules > question > answer > check > result'''
-    user = welcome_user()
+    print('Welcome to the Brain Games!')
+    name = prompt.string('May I have your name? ')
+    print(f'Hello, {name}!')
     print(game_module.START_MESSAGE)
-    CORRECT = 0
-    while CORRECT < 3:
-        question_and_answer = game_module.question_checker()
-        guess = question_and_answer[0]
+    Correct = 0
+    while Correct < 3:
+        guess, correct_answer = game_module.make_question_and_answer()
         print(f'Question: {guess}')
         user_answer = prompt.string('Your answer: ')
-        correct_answer = question_and_answer[1]
         if correct_answer == user_answer:
-            CORRECT += 1
+            Correct += 1
             print('Correct!')
         else:
             print(f"'{user_answer}' is wrong answer ;(. "
                   f"Correct answer was '{correct_answer}'.")
-            print(f"Let's try again, {user}!")
+            print(f"Let's try again, {name}!")
             break
-    if CORRECT == 3:
-        print(f'Congratulations, {user}!')
+    if Correct == 3:
+        print(f'Congratulations, {name}!')

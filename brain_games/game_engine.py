3328580182dbfb2fc8fb547f733a78ca31e7ebd8
minami-11 <madk0te@yandex.ru>
2023-02-13 14:51:16 +0500

    Fix: rename Correct to correct in game_engine.py

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -7,18 +7,18 @@ def create_gameplay(game_module):
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     print(game_module.START_MESSAGE)
-    Correct = 0
-    while Correct < 3:
+    correct = 0
+    while correct < 3:
         guess, correct_answer = game_module.make_question_and_answer()
         print(f'Question: {guess}')
         user_answer = prompt.string('Your answer: ')
         if correct_answer == user_answer:
-            Correct += 1
+            correct += 1
             print('Correct!')
         else:
             print(f"'{user_answer}' is wrong answer ;(. "
                   f"Correct answer was '{correct_answer}'.")
             print(f"Let's try again, {name}!")
             break
-    if Correct == 3:
+    if correct == 3:
         print(f'Congratulations, {name}!')

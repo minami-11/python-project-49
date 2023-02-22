fa69d470c655801a63ad0edd9c3674e35497e3aa
minami-11 <madk0te@yandex.ru>
2023-02-22 17:33:56 +0500

    Refactor: game_engine.py due to review_refactor v.4.1
    Replace the while loop with a for loop. Delete "correct" variable.

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -7,18 +7,16 @@ def create_gameplay(game_module):
     name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
     print(game_module.START_MESSAGE)
-    correct = 0
-    while correct < 3:
+    for round_number in ('1_round', '2_round', '3_round'):
         guess, correct_answer = game_module.make_question_and_answer()
         print(f'Question: {guess}')
         user_answer = prompt.string('Your answer: ')
         if correct_answer == user_answer:
-            correct += 1
             print('Correct!')
+            if round_number == '3_round':
+                print(f'Congratulations, {name}!')
         else:
             print(f"'{user_answer}' is wrong answer ;(. "
                   f"Correct answer was '{correct_answer}'.")
             print(f"Let's try again, {name}!")
             break
-    if correct == 3:
-        print(f'Congratulations, {name}!')

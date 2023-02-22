8514172a19331dcd22157513cd8a9e4ae6cb8eea
minami-11 <madk0te@yandex.ru>
2023-02-22 17:28:47 +0500

    Refactor: progression.py due to review_refactor v.4.1
    Move genarating arithmetic progression logic part to new function
    "generate_progression". Rename "arithm_progress" to "arithmetic_progres-
    sion".

--- brain_games/games/progression.py
+++ brain_games/games/progression.py
@@ -10,6 +10,15 @@ STEP_MIN = 2
 STEP_MAX = 10
 
 
+def generate_progression(length: int, start_point: int, step: int) -> list:
+    '''Generates random arithmetic progression'''
+    arithm_progress = []
+    for _ in range(length):
+        arithm_progress.append(str(start_point))
+        start_point += step
+    return arithm_progress
+
+
 def cut_number_from_progression(progression: list) -> list:
     '''Cut number from progression and return them back'''
     guess_number = choice(progression)
@@ -22,9 +31,7 @@ def make_question_and_answer() -> list:
     progress_length = randint(LENGTH_MIN, LENGTH_MAX)
     progress_point = randint(MIN_LIMIT, MAX_LIMIT)
     progress_step = randint(STEP_MIN, STEP_MAX)
-    arithm_progress = []
-    for _ in range(progress_length):
-        arithm_progress.append(str(progress_point))
-        progress_point += progress_step
-    question, answer = cut_number_from_progression(arithm_progress)
+    arithmetic_progression = generate_progression(progress_length,
+                                                  progress_point, progress_step)
+    question, answer = cut_number_from_progression(arithmetic_progression)
     return [question, answer]

cb1882b173f1a592242a56774a2d72b222a8092c
minami-11 <madk0te@yandex.ru>
2023-02-22 17:17:23 +0500

    Update: calculate.py due to review_refactor v.4.1
    Rename function from "convert_string_to_math" to "calculate_expression".

--- brain_games/games/calculate.py
+++ brain_games/games/calculate.py
@@ -6,7 +6,7 @@ MIN_LIMIT = 0
 MAX_LIMIT = 50
 
 
-def convert_string_to_math(number_1: int, number_2: int, math_operator: str):
+def calculate_expression(number_1: int, number_2: int, math_operator: str):
     '''Converts string to math expression with end result'''
     action_list = {'+': int(number_1) + int(number_2),
                    '-': int(number_1) - int(number_2),
@@ -21,5 +21,5 @@ def make_question_and_answer() -> list:
     math_actions = ['+', '-', '*']
     math_operator = choice(math_actions)
     question = f'{number_1} {math_operator} {number_2}'
-    answer = str(convert_string_to_math(number_1, number_2, math_operator))
+    answer = str(calculate_expression(number_1, number_2, math_operator))
     return [question, answer]
